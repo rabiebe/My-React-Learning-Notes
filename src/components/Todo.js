e355734afe4e02711c6cf88ce2f0ced7069fd153
rabie belkaida <rb@omniyat.ma>
2024-02-26 22:26:17 +0100

    Add Todo component

---
+++ src/components/Todo.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { title: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+  const toggleTodoCompletion = (index) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="Todo">
+      <h1>Todo List</h1>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodoCompletion(index)}
+            />
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
